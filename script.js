f08725423e43d82fb5bc39b15f75a0cfd87f9194
alessandropecchini99 <pecchini99@gmail.com>
2023-04-24 13:56:41 +0200

    start bonus

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-/* CAMPO_MINATO */
-// 1. L'utente clicca su un bottone
+/* CAMPO_MINATO - ESERCIZIO NORMALE */
+// 1. L'utente clicca su play
 // 2. Che genererà una griglia di gioco quadrata 8x8
 // 3. Quando l'utente clicca su ogni cella... 
 // 4. ...la cella cliccata si colora di azzurro
 // 5. Emetto un messaggio in console con il numero della cella cliccata
 
 
-
-// 1. BUTTON
+/* 1. BUTTON
 const btnPlay = document.querySelector(`#play`);
 btnPlay.addEventListener(`click`, 
     function() {
@@ -31,6 +30,48 @@ btnPlay.addEventListener(`click`,
                 }
             )
         };
+
+    }
+) */
+
+
+// ------------------------------
+
+
+/* CAMPO_MINATO - ESERCIZIO CON BONUS */
+// 1. L'utente sceglie la difficoltà e clicca su play
+// 2. Che genererà una griglia di gioco quadrata in base alla difficoltà (easy 7x7 / medium 9x9 / hard 10x10)
+// 3. Quando l'utente clicca su ogni cella... 
+// 4. ...la cella cliccata si colora di azzurro
+// 5. Emetto un messaggio in console con il numero della cella cliccata
+
+
+// 1. BUTTON
+const btnPlay = document.querySelector(`#play`);
+btnPlay.addEventListener(`click`, 
+    function() {
+        
+        // 2. GENERO GRIGLIA
+        const grid = document.querySelector(`.grid`);
+        document.querySelector(`.grid`).innerHTML = "";
+
+        let difficult = document.querySelector(`#difficult`).value.toLowerCase().replace(" ", "");
+        console.log(difficult);
+
+        // controllo difficoltà
+        if (difficult == `easy`) {
+            for (let i = 1; i <= 49; i++) {
+                grid.innerHTML += `<div class="cell_7">${i}</div>`;
+            };
+        } else if (difficult == `medium`) {
+            for (let i = 1; i <= 81; i++) {
+                grid.innerHTML += `<div class="cell_9">${i}</div>`;
+            };
+        } else if (difficult == `hard`) {
+            for (let i = 1; i <= 100; i++) {
+                grid.innerHTML += `<div class="cell_10">${i}</div>`;
+            };
+        }
 
     }
 )
